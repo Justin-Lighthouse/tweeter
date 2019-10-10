afebe379ce4789d2483f2ab162795d7417cbf944
Justin Stewart <Justin.lighthouse.labs@gmail.com>
2019-10-10 16:37:37 +0000

    tweets will be added to the list of tweets and update after each submission using ajax so the user does not need to refresh the page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,80 +3,27 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
-// $(document).ready(function() {
-//   const tweetData = {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png",
-//         "handle": "@SirIsaac"
-//       },
-//     "content": {
-//         "text": "If I have seen further it is by standing on the shoulders of giants"
-//       },
-//     "created_at": 1461116232227
-//   }
-  
-  // const createTweetElement = function(tweet) {
-  //   let $tweet = $('<article>').addClass('tweet');
-  
-  //   $($tweet).html(
-  //   `<article>
-  //     <header>
-  //       <img class="avatars" src=${tweet.user.avatars}>
-  //       <span class="name">${tweet.user.name}</span>
-  //       <span class="name handle">${tweet.user.handle}</span>
-  //     </header>
-  //     <p class="content">${tweet.content.text}</p>
-  //     <footer class="tweet-footer">
-  //       <span class="created_at" >${tweet.created_at}</span>
-  //       <span class="flags">LIKE ME!!!</span>
-  //     </footer>
-  //   </article>`
-  //   );
-  
-  //   return $tweet;
-  // }
-  
-//   const $tweet = createTweetElement(tweetData);
-//   console.log(typeof $tweet)
-//   // Test / driver code (temporary)
-//   console.log($tweet); // to see what it looks like
-//   $('#tweets-container').append($tweet);
-// });
-
-
 $(document).ready(function() {
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
   
   const renderTweets = function(tweets) {
   // loops through tweets
   // calls createTweetElement for each tweet
   // takes return value and appends it to the tweets container
-    $('#tweets-container').append(tweets.map(tweet => createTweetElement(tweet)));
+
+    for (const tweet of tweets) {
+      $("#tweets-container").prepend(createTweetElement(tweet))
+    }
+    // $('#tweets-container').prepend(tweets.map(tweet => createTweetElement(tweet)));
+  }
+
+  $("#toggle").click(function() {
+    $("#newTweet").slideToggle( "slow" );
+  });
+
+  const escape =  function(str) {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
   }
   
   const createTweetElement = function(tweet) {
@@ -89,7 +36,7 @@ $(document).ready(function() {
         <span class="name">${tweet.user.name}</span>
         <span class="name handle">${tweet.user.handle}</span>
       </header>
-      <p class="content">${tweet.content.text}</p>
+      <p class="content">${escape(tweet.content.text)}</p>
       <footer class="tweet-footer">
         <span class="created_at" >${tweet.created_at}</span>
         <span class="flags">LIKE ME!!!</span>
@@ -99,6 +46,36 @@ $(document).ready(function() {
   
     return $tweet;
   }
-  
-  renderTweets(data);
+
+  $('#newTweet').submit(function(event) {
+    event.preventDefault();
+    if($($('#myTextbox')).val().length === 0) {
+      $(".alert").text("Oops, your tweet is empty!")
+      $(".alert").slideToggle( "slow" );
+    } else if($($('#myTextbox')).val().length > 140) {
+      $(".alert").text("Oops, your tweet exceeds the max character length!")
+      $(".alert").slideToggle( "slow" );
+    } else {
+      const data = $($('#myTextbox')).serialize();
+      $.ajax({
+        type: "POST",
+        url: '/tweets',
+        data: data
+      })
+      .then((res) => {
+        $('textarea').val('');
+        $('.counter').text(140);
+        loadTweets();
+      })
+    }
+  })
+
+  const loadTweets = function() {
+    $.ajax('/tweets', {method: "GET"})
+    .then((res) => {
+      renderTweets(res);
+    })
+  };
+
+  loadTweets();
 });
