c5e38bd6a6defb337be528d43868c119e4cca5ae
Justin Stewart <Justin.lighthouse.labs@gmail.com>
2019-10-11 00:25:53 +0000

    shows red number if over char length

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
   const maxTweetLength = 140;
   $("#myTextbox").keyup(function() {
     const counter = $(this).val().length;
+    if (maxTweetLength - counter < 0) {
+      $(".counter").addClass("tooBig")
+    }
     $($(this).siblings("span")[0]).text(maxTweetLength - counter);
   });
 });
