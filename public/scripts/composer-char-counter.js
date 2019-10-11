8a3e24bc6fb268bb56c7800fb0b9925c456b87a2
Justin Stewart <Justin.lighthouse.labs@gmail.com>
2019-10-11 01:33:52 +0000

    fixed bug where counter stayed red after being negative

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,6 +4,8 @@ $(document).ready(function() {
     const counter = $(this).val().length;
     if (maxTweetLength - counter < 0) {
       $(".counter").addClass("tooBig")
+    } else {
+      $(".counter").removeClass("tooBig")
     }
     $($(this).siblings("span")[0]).text(maxTweetLength - counter);
   });
